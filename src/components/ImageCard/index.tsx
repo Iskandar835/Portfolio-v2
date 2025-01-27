172fad9a0792467d39454c0d7fabad2b14286384
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-27 14:05:57 +0100

    Ajout composants ImageCard et TextCard avec leurs styles/ Rename fichier css de PurpleShadow/ Ajout image de test gabarie-card

---
+++ src/components/ImageCard/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function ImageCard() {
+   return (
+      <div className="image-card-container">
+         <img src="/assets/gabarie-card.png" alt="Image du projet" />
+      </div>
+   );
+}
+
+export default ImageCard;
---
+++ src/components/TextCard/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+function TextCard() {
+   return (
+      <div className="text-card-container">
+         <p>
+            A web app for visualizing personalized Spotify data. View your top
+            artists, top tracks, recently played tracks, and detailed audio
+            information about each track. Create and save new playlists of
+            recommended tracks based on your existing playlists and more.
+         </p>
+      </div>
+   );
+}
+
+export default TextCard;
