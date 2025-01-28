dbf79f6c84d9ec5f5a7ae8c5c6e39bb75700fce3
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-28 17:11:56 +0100

    Ajout composant TechnoLogo avec son style/ Ajout d'image de 4 technos

---
+++ src/components/TechnoLogo/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function TechnoLogo() {
+   return (
+      <div className="logo-container">
+         <img src="/assets/logo-react.png" alt="Logo de la technologie" />
+      </div>
+   );
+}
+
+export default TechnoLogo;
