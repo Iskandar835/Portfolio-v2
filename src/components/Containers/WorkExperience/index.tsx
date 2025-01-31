3f43048c7324515566fd0c5f627a09b8d276de1c
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-31 19:39:23 +0100

    feat: Ajout fonction de trie par date + map de la database pour affichage dynamique de WorkCard

--- src/components/Containers/WorkExperience/index.tsx
+++ src/components/Containers/WorkExperience/index.tsx
@@ -1,15 +1,25 @@
 import "./styles.css";
+import data from "../../../data/database.json";
 import WorkCard from "../../WorkCard";
 import PurpleShadow from "../../PurpleShadow";
 
 function WorkExperience() {
+   const sortExperience = data?.works.sort((pjtA, pjtB) =>
+      new Date(pjtA.date) > new Date(pjtB.date) ? -1 : 1
+   );
+
    return (
       <>
          <div className="all-work-card">
-            <WorkCard />
-            <WorkCard />
-            <WorkCard />
-            <WorkCard />
+            {sortExperience.map((work) => (
+               <WorkCard
+                  key={work.id}
+                  source={work.picture.source}
+                  alt={work.picture.alt}
+                  title={work.name}
+                  story={work.story}
+               />
+            ))}
          </div>
          <div className="work-shadow-container">
             <PurpleShadow isLargeVersion={true} />
