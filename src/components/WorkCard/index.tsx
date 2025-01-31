25d429f05a13b1811b406218d8ce00040882b28e
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-31 11:25:56 +0100

    chore: WorkCard optimiser avec son style

--- src/components/WorkCard/index.tsx
+++ src/components/WorkCard/index.tsx
@@ -1,7 +1,22 @@
 import "./styles.css";
+import Button from "../Button";
 
 function WorkCard() {
-   return <></>;
+   return (
+      <article className="work-card-container">
+         <img src="/assets/i-work.png" alt="" />
+         <div className="work-card-content">
+            <h3>CIB on the Mobile</h3>
+            <p>
+               Take your client onboard seamlessly by our amazing tool of
+               digital onboard process.
+            </p>
+            <div className="work-card-btn-container">
+               <Button />
+            </div>
+         </div>
+      </article>
+   );
 }
 
 export default WorkCard;
