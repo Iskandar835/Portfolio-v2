6d3115a48e44e26b6e41f8e175ab55eb6e9e934d
Sacha Vandermoeten <sachadu835@gmail.com>
2025-01-31 14:42:51 +0100

    chore: construction de WorkExperience avec son style

--- src/components/Containers/WorkExperience/index.tsx
+++ src/components/Containers/WorkExperience/index.tsx
@@ -1,7 +1,21 @@
 import "./styles.css";
+import WorkCard from "../../WorkCard";
+import PurpleShadow from "../../PurpleShadow";
 
 function WorkExperience() {
-   return <></>;
+   return (
+      <>
+         <div className="all-work-card">
+            <WorkCard />
+            <WorkCard />
+            <WorkCard />
+            <WorkCard />
+         </div>
+         <div className="work-shadow-container">
+            <PurpleShadow isLargeVersion={true} />
+         </div>
+      </>
+   );
 }
 
 export default WorkExperience;
