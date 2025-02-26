b662d92c065026912db1dbf41f8226fc08e024f3
Sacha Vandermoeten <sachadu835@gmail.com>
2025-02-26 18:05:46 +0100

    feat: ajout composant pellet optimiser avec son style

---
+++ src/components/Pellet/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function Pellet() {
+   return (
+      <div className="pellet">
+         <span className="normal"></span>
+         <span className="blink"></span>
+      </div>
+   );
+}
+
+export default Pellet;
