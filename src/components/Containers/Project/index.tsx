b2d20acb3eed77758e9702ab4ef5120e0c127349
Sacha Vandermoeten <sachadu835@gmail.com>
2025-02-26 18:08:55 +0100

    fix: correction bug scroll animation

--- src/components/Containers/Project/index.tsx
+++ src/components/Containers/Project/index.tsx
@@ -26,6 +26,7 @@ function Project({
       <motion.article
          initial={{ opacity: 0, scale: 0 }}
          whileInView={{ opacity: 1, scale: 1 }}
+         transition={{ duration: 0.5 }}
          className={indexIsEven ? "project-article" : "project-article-odd"}
       >
          <section className="project-description">
